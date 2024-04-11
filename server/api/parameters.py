a80e498d2895d113b1f67ae72968c12e7198d805
glowingkitty <contact@glowingkitty.com>
2024-04-11 14:11:08 +0200

    improve readability

---
+++ server/api/parameters.py
@@ -0,0 +1,40 @@
+
+tags_metadata = [
+    {
+        "name": "Mates",
+        "description": "<img src='images/mates.png' alt='Mates are your AI team members. They can help you with various tasks. Each mate is specialized in a different area.'>"
+    },
+    {
+        "name": "Skills",
+        "description": "<img src='images/skills.png' alt='Your team mate can use a wide range of skills. Or, you can call them directly via the API. For example: ChatGPT, StableDiffusion, Notion or Figma.'>"
+    },
+    {
+        "name": "Workflows",
+        "description": "<img src='images/workflows.png' alt='A skill is powerful. Combining multiple skills in a workflow is magical.'>"
+    },
+    {
+        "name": "Tasks",
+        "description": "<img src='images/tasks.png' alt='A task is a scheduled run of a single skill or a whole workflow. It can happen once, or repeated.'>"
+    },
+    {
+        "name": "Billing",
+        "description": "<img src='images/billing.png' alt='Manage your billing settings, download invoices and more.'>"
+    },
+    {
+        "name": "Server",
+        "description": "<img src='images/server.png' alt='Manage your OpenMates server. Change settings, see how the server is doing and more.'>"
+    },
+    {
+        "name": "Teams",
+        "description": "<img src='images/teams.png' alt='Manage the teams on your OpenMates server.'>"
+    },
+    {
+        "name": "Users",
+        "description": "<img src='images/users.png' alt='Manage user accounts of a team. A user can get personalized responses from mates and access to the OpenMates API.'>"
+    }
+]
+
+input_parameter_descriptions = {
+    "team_url": "Your team URL to access the requested team.",
+    "token": "Your API token to authenticate and show you have access to the requested team."
+}
