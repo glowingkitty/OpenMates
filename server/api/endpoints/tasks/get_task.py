b08358b1395985c4c325c4a28860a80ed7133fa2
glowingkitty <contact@glowingkitty.com>
2024-08-29 11:46:46 +0200

    clean up a bit

--- server/api/endpoints/tasks/get_task.py
+++ server/api/endpoints/tasks/get_task.py
@@ -26,12 +26,4 @@ async def get_task(task_id: str) -> TasksGetTaskOutput:
     if not task_result:
         raise HTTPException(status_code=404, detail="Task not found")
 
-    add_to_log(f"Task result: {task_result.result}")
-
-    return TasksGetTaskOutput(
-        id=task_id,
-        title="Unknown",
-        status=task_result.state.lower(),
-        output=task_result.result
-    )
-
+    return task_result.result
--- server/api/endpoints/tasks/tasks.py
+++ server/api/endpoints/tasks/tasks.py
@@ -23,8 +23,6 @@ def ask_mate_task(team_slug, message, mate_username, task_info):
     # Run the async function in an event loop
     loop = asyncio.get_event_loop()
     response = loop.run_until_complete(ask_mate_processing(team_slug, message, mate_username))
-    add_to_log(f"Response: {response}")
-    add_to_log(f"Response type: {type(response)}")
     response_dict = response.model_dump()
     # Return the task output
     return TasksGetTaskOutput(
