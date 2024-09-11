1bb6f841ca73cc3c551836d4f6245801fb5a7087
glowingkitty <contact@glowingkitty.com>
2024-09-11 18:03:03 +0200

    added memory usage to validate_user_data_access

--- server/api/validation/validate_user_data_access.py
+++ server/api/validation/validate_user_data_access.py
@@ -17,6 +17,34 @@ from fastapi import HTTPException
 from server.cms.strapi_requests import make_strapi_request, get_nested
 from typing import Union, Literal
 from server.api.security.crypto import verify_hash
+from server.api.memory import get_user_from_memory, save_user_to_memory
+
+def check_user_access(user, request_team_slug, request_endpoint, username):
+    if request_endpoint == "get_one_user":
+        if username and get_nested(user, "username") == username:
+            add_to_log("User is trying to access its own data.")
+            return "full_access"
+        if get_nested(user, "is_server_admin"):
+            add_to_log("User is a server admin and has the permission to access all basic user data.")
+            return "basic_access"
+        if request_team_slug and get_nested(user, "teams_where_user_is_admin"):
+            for team in get_nested(user, "teams_where_user_is_admin"):
+                if get_nested(team, "slug") == request_team_slug:
+                    add_to_log("User is a team admin and has the permission to access all basic user data on the team.")
+                    return "basic_access"
+        add_to_log("User does not have the permission to access the user data.")
+        raise HTTPException(status_code=404, detail="User not found. Either the username is wrong, the team slug is wrong or the user is not part of the team or you don't have the permission to access this user.")
+    elif request_endpoint == "get_all_users":
+        if get_nested(user, "is_server_admin"):
+            add_to_log("User is a server admin and has the permission to access all basic user data.")
+            return "basic_access_for_all_users_on_server"
+        if request_team_slug and get_nested(user, "teams_where_user_is_admin"):
+            for team in get_nested(user, "teams_where_user_is_admin"):
+                if get_nested(team, "slug") == request_team_slug:
+                    add_to_log("User is a team admin and has the permission to access all basic user data on the team.")
+                    return "basic_access_for_all_users_on_team"
+        add_to_log("User is only allowed to access its own data.")
+        return "basic_access_for_own_user_only"
 
 
 async def validate_user_data_access(
@@ -35,10 +63,14 @@ async def validate_user_data_access(
 
         filters = []
 
-        # if user api_token is given, get uid and api_token and filter based on uid and check for api_token later
         if token:
             uid = token[:32]
             api_token = token[32:]
+
+            user = get_user_from_memory(uid)
+            if user:
+                if verify_hash(get_nested(user, "api_token"), api_token):
+                    return check_user_access(user, request_team_slug, request_endpoint, username)
 
             filters.append({
                 "field": "uid",
@@ -52,7 +84,6 @@ async def validate_user_data_access(
                 "value": username
             })
 
-        # get the userdata for the user who makes the request, based on the token
         status_code, json_response = await make_strapi_request(
             method='get',
             endpoint='user-accounts',
@@ -65,63 +96,17 @@ async def validate_user_data_access(
             raise HTTPException(status_code=404, detail="User not found.")
 
         user = json_response["data"][0]
+        save_user_to_memory(uid, json_response)
 
-        # if the api_token is given, check if it matches the users api_token
         if token and not verify_hash(get_nested(user, "api_token"), api_token):
             add_to_log("The user token is invalid.", module_name="OpenMates | API | Validate user data Access", state="error")
             raise HTTPException(status_code=403, detail="The user token is invalid")
 
-        # if the password is given, check if it matches the users passwor
         if password and not verify_hash(get_nested(user, "password"), password):
             add_to_log("The password invalid.", module_name="OpenMates | API | Validate user data Access", state="error")
             raise HTTPException(status_code=403, detail="The password is invalid")
 
-        # check if the user has the righ to acces the
-        if request_endpoint == "get_one_user":
-            add_to_log("Checking if the user has the permission to access a specific user ...")
-
-            # check if the found user has the same username as in username
-            if username and get_nested(user, "username") == username:
-                # if so that means a user tries to get its own data and we can proceed with 'full data' access
-                add_to_log("User is trying to access its own data.")
-                return "full_access"
-
-            # else we check if the user is marked as a server admin, if so, we can proceed 'basic data' access
-            if get_nested(user, "is_server_admin"):
-                add_to_log("User is a server admin and has the permission to access all basic user data.")
-                return "basic_access"
-
-            # else we check if the user is a team admin, if so, we can proceed with 'basic data' access
-            for team in get_nested(user, "teams_where_user_is_admin"):
-                if get_nested(team, "slug") == request_team_slug:
-                    # user is team admin and therefore is allowed to access the 'basic data' of all users on the team
-                    add_to_log("User is a team admin and has the permission to access all basic user data on the team.")
-                    return "basic_access"
-
-            # else, the user is not allowed to access the data
-            add_to_log("User does not have the permission to access the user data.")
-            raise HTTPException(status_code=404, detail="User not found. Either the username is wrong, the team slug is wrong or the user is not part of the team or you don't have the permission to access this user.")
-
-
-        if request_endpoint == "get_all_users":
-            add_to_log("Checking if the user has the permission to access all users ...")
-            # check if the found user is a server admin, if so, we can proceed with 'basic data' access
-            if get_nested(user, "is_server_admin"):
-                add_to_log("User is a server admin and has the permission to access all basic user data.")
-                return "basic_access_for_all_users_on_server"
-
-            # check if user is a team admin, if so, we can proceed with 'basic data' access
-            for team in get_nested(user, "teams_where_user_is_admin"):
-                if get_nested(team, "slug") == request_team_slug:
-                    # user is team admin and therefore is allowed to access the 'basic data' of all users on the team
-                    add_to_log("User is a team admin and has the permission to access all basic user data on the team.")
-                    return "basic_access_for_all_users_on_team"
-
-            # else the user is only allowed to access the 'basic data' of the user itself
-            add_to_log("User is only allowed to access its own data.")
-            return "basic_access_for_own_user_only"
-
-
+        return check_user_access(user, request_team_slug, request_endpoint, username)
 
     except HTTPException:
         raise
