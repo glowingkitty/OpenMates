9ce638a4aa7ecf1343af977ca3348bd43055207d
glowingkitty <contact@glowingkitty.com>
2024-12-13 11:05:39 +0100

    updated css urls

--- server/website_svelte/vite.config.ts
+++ server/website_svelte/vite.config.ts
@@ -1,6 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			// Alias for mate images pointing to the API images folder
+			'@mate-images': path.resolve(__dirname, '../api/images/mates/profile_images')
+		}
+	},
+	server: {
+		fs: {
+			// Allow serving files from one level up to include the api folder
+			allow: [
+				// Defaults
+				'src',
+				'.svelte-kit',
+				// Add api directory to allow list
+				path.resolve(__dirname, '../api')
+			]
+		}
+	}
 });
