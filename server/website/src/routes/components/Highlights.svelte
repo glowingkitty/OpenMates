c457200b31c8078a57af9923b484b7af3fdf181f
glowingkitty <contact@glowingkitty.com>
2024-12-23 12:33:52 +0100

    added large seperator class

--- server/website/src/routes/components/Highlights.svelte
+++ server/website/src/routes/components/Highlights.svelte
@@ -24,29 +24,3 @@
     text_side="left"
 />
 {/if}
-
-<!-- chat_example=[
-                    {
-                        type: 'user',
-                        text: 'I am unhappy in my current job. Any ideas in what direction I could go instead?',
-                        waitTime: 2000
-                    },
-                    {
-                        type: 'started_focus',
-                        appNames: ['Jobs'],
-                        focusName: 'Career insights',
-                        focusIcon: 'insights',
-                        waitTime: 500
-                    },
-                    {
-                        type: 'loaded_preferences',
-                        appNames: ['Jobs'],
-                        waitTime: 500
-                    },
-                    {
-                        type: 'mate',
-                        mateName: 'Burton',
-                        text: 'Of course! Since you mentioned that you have a background in marketing and enjoy storytelling, we could look for roles that leverage those skills.\n\nTo get a better sense of direction, could you tell me:\n1. What aspects of your previous jobs did you find most fulfilling?',
-                        waitTime: 3000
-                    }
-                ] -->
---
+++ server/website/src/routes/components/LargeSeparator.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    // Props for configuring the separator's appearance
+    export let reverse_direction: boolean = false;
+</script>
+
+<div class="large-separator" style={reverse_direction ? "rotate: 180deg" : ""}>
+    <div class="separator-content"></div>
+</div>
