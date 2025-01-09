d9a802118870018a79f7e45ae282e4917a059f12
glowingkitty <contact@marcobartsch.me>
2025-01-09 21:20:03 +0100

    added first icons to activityhistory

---
+++ server/website/src/routes/components/ActivityHistory.svelte
@@ -0,0 +1,33 @@
+<button 
+    class="clickable-icon icon_search top-button left" 
+    aria-label="Search"
+></button>
+<button 
+    class="clickable-icon icon_filter top-button center" 
+    aria-label="Filter"
+></button>
+<button 
+    class="clickable-icon icon_close top-button right" 
+    aria-label="Close"
+></button>
+
+<style>
+    /* Position buttons in the top bar */
+    .top-button {
+        position: absolute;
+        top: 20px;
+    }
+
+    .top-button.left {
+        left: 20px;
+    }
+
+    .top-button.center {
+        left: 50%;
+        transform: translateX(-50%); /* Center the button horizontally */
+    }
+
+    .top-button.right {
+        right: 20px;
+    }
+</style>
