512508a0d547aa7f39c95c14333558dc580d4e96
glowingkitty <contact@marcobartsch.me>
2025-01-10 21:18:58 +0100

    fixed loading issues with translation

--- server/website/src/routes/components/Highlights.svelte
+++ server/website/src/routes/components/Highlights.svelte
@@ -1,53 +1,56 @@
 <script lang="ts">
     import Highlight from './Highlight.svelte';
-    import { _ } from 'svelte-i18n';
+    import { _, waitLocale } from 'svelte-i18n';
     export let target: string = '';
 </script>
 
-<!-- Add all highlights here -->
-{#if target === 'for_all'}
-<Highlight
-    sub_heading="Ask"
-    main_heading={`<mark>${$_('highlight.sections.ask.main_heading_1.text')}</mark><br>${$_('highlight.sections.ask.main_heading_2.text')}`}
-    paragraph={`${$_('highlight.sections.ask.paragraph.text')}`}
-    text_side="left"
-    {target}
-/>
-<Highlight
-    sub_heading="Tasks"
-    main_heading={`${$_('highlight.sections.tasks.main_heading_1.text')}<br><mark>${$_('highlight.sections.tasks.main_heading_2.text')}</mark>`}
-    paragraph={`${$_('highlight.sections.tasks.paragraph.text')}`}
-    text_side="right"
-    {target}
-/>
-<Highlight
-    sub_heading="Apps"
-    main_heading={`<mark>${$_('highlight.sections.apps.main_heading_1.text')}</mark><br>${$_('highlight.sections.apps.main_heading_2.text')}`}
-    paragraph={`${$_('highlight.sections.apps.paragraph.text')}`}
-    text_side="left"
-    {target}
-/>
-{/if}
-{#if target === 'for_developers'}
-<Highlight
-    sub_heading="Ask"
-    main_heading={`<mark>${$_('highlight.sections.ask.main_heading.text')}</mark>`}
-    paragraph={`${$_('highlight.sections.ask.paragraph.text')}`}
-    text_side="left"
-    target="for_developers"
-/>
-<Highlight
-    sub_heading="Tasks"
-    main_heading={`<mark>${$_('highlight.sections.tasks.main_heading.text')}</mark>`}
-    paragraph={`${$_('highlight.sections.tasks.paragraph.text')}`}
-    text_side="right"
-    target="for_developers"
-/>
-<Highlight
-    sub_heading="Apps"
-    main_heading={`<mark>${$_('highlight.sections.apps.main_heading.text')}</mark>`}
-    paragraph={`${$_('highlight.sections.apps.paragraph.text')}`}
-    text_side="left"
-    target="for_developers"
-/>
-{/if}
+{#await waitLocale()}
+{:then}
+    {#if target === 'for_all'}
+        <Highlight
+            sub_heading="Ask"
+            main_heading={`<mark>${$_('highlight.sections.ask.main_heading_1.text')}</mark><br>${$_('highlight.sections.ask.main_heading_2.text')}`}
+            paragraph={$_('highlight.sections.ask.paragraph.text')}
+            text_side="left"
+            {target}
+        />
+        <Highlight
+            sub_heading="Tasks"
+            main_heading={`${$_('highlight.sections.tasks.main_heading_1.text')}<br><mark>${$_('highlight.sections.tasks.main_heading_2.text')}</mark>`}
+            paragraph={$_('highlight.sections.tasks.paragraph.text')}
+            text_side="right"
+            {target}
+        />
+        <Highlight
+            sub_heading="Apps"
+            main_heading={`<mark>${$_('highlight.sections.apps.main_heading_1.text')}</mark><br>${$_('highlight.sections.apps.main_heading_2.text')}`}
+            paragraph={$_('highlight.sections.apps.paragraph.text')}
+            text_side="left"
+            {target}
+        />
+    {/if}
+
+    {#if target === 'for_developers'}
+        <Highlight
+            sub_heading="Ask"
+            main_heading={`<mark>${$_('highlight.sections.ask.main_heading.text')}</mark>`}
+            paragraph={$_('highlight.sections.ask.paragraph.text')}
+            text_side="left"
+            target="for_developers"
+        />
+        <Highlight
+            sub_heading="Tasks"
+            main_heading={`<mark>${$_('highlight.sections.tasks.main_heading.text')}</mark>`}
+            paragraph={$_('highlight.sections.tasks.paragraph.text')}
+            text_side="right"
+            target="for_developers"
+        />
+        <Highlight
+            sub_heading="Apps"
+            main_heading={`<mark>${$_('highlight.sections.apps.main_heading.text')}</mark>`}
+            paragraph={$_('highlight.sections.apps.paragraph.text')}
+            text_side="left"
+            target="for_developers"
+        />
+    {/if}
+{/await}
