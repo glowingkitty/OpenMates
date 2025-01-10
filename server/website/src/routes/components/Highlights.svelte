b078297154335dda7cfa9dbd390c82be2023791e
glowingkitty <contact@marcobartsch.me>
2025-01-10 20:58:16 +0100

    improved translations

--- server/website/src/routes/components/Highlights.svelte
+++ server/website/src/routes/components/Highlights.svelte
@@ -8,22 +8,22 @@
 {#if target === 'for_all'}
 <Highlight
     sub_heading="Ask"
-    main_heading={`<mark>${$_('highlight.sections.ask.main_heading.text')}</mark>`}
-    paragraph={$_('highlight.sections.ask.paragraph.text')}
+    main_heading={`<mark>${$_('highlight.sections.ask.main_heading_1.text')}</mark><br>${$_('highlight.sections.ask.main_heading_2.text')}`}
+    paragraph={`${$_('highlight.sections.ask.paragraph.text')}`}
     text_side="left"
     {target}
 />
 <Highlight
     sub_heading="Tasks"
-    main_heading={`<mark>${$_('highlight.sections.tasks.main_heading.text')}</mark>`}
-    paragraph={$_('highlight.sections.tasks.paragraph.text')}
+    main_heading={`${$_('highlight.sections.tasks.main_heading_1.text')}<br><mark>${$_('highlight.sections.tasks.main_heading_2.text')}</mark>`}
+    paragraph={`${$_('highlight.sections.tasks.paragraph.text')}`}
     text_side="right"
     {target}
 />
 <Highlight
     sub_heading="Apps"
     main_heading={`<mark>${$_('highlight.sections.apps.main_heading.text')}</mark>`}
-    paragraph={$_('highlight.sections.apps.paragraph.text')}
+    paragraph={`${$_('highlight.sections.apps.paragraph.text')}`}
     text_side="left"
     {target}
 />
@@ -32,21 +32,21 @@
 <Highlight
     sub_heading="Ask"
     main_heading={`<mark>${$_('highlight.sections.ask.main_heading.text')}</mark>`}
-    paragraph={$_('highlight.sections.ask.paragraph.text')}
+    paragraph={`${$_('highlight.sections.ask.paragraph.text')}`}
     text_side="left"
     target="for_developers"
 />
 <Highlight
     sub_heading="Tasks"
     main_heading={`<mark>${$_('highlight.sections.tasks.main_heading.text')}</mark>`}
-    paragraph={$_('highlight.sections.tasks.paragraph.text')}
+    paragraph={`${$_('highlight.sections.tasks.paragraph.text')}`}
     text_side="right"
     target="for_developers"
 />
 <Highlight
     sub_heading="Apps"
     main_heading={`<mark>${$_('highlight.sections.apps.main_heading.text')}</mark>`}
-    paragraph={$_('highlight.sections.apps.paragraph.text')}
+    paragraph={`${$_('highlight.sections.apps.paragraph.text')}`}
     text_side="left"
     target="for_developers"
 />
