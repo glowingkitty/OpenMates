f32679fcbeea1322f92343ce868e2636d9ed0b69
glowingkitty <contact@marcobartsch.me>
2025-01-10 19:03:48 +0100

    web_app works, desite error in layout

--- server/web_app/src/routes/+layout.js
+++ server/web_app/src/routes/+layout.js
@@ -1,16 +1,8 @@
 import '@fontsource-variable/lexend-deca';
-import { browser } from '$app/environment';
-import { init, register } from 'svelte-i18n';
+import { initI18n } from '@website-i18n/setup';
 
 export const prerender = true;
 export const ssr = true;
 
-
-
-register('en', () => import('../locales/en.json'));
-register('de', () => import('../locales/de.json'));
-
-init({
-    fallbackLocale: 'en',
-    initialLocale: browser ? window.navigator.language : 'en',
-}); 
+// Initialize i18n
+initI18n(); 
