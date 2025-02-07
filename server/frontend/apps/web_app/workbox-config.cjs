92ab74c28e0c199e32a3f2ba49c54e9888741d1f
glowingkitty <contact@marcobartsch.me>
2025-02-07 16:00:02 +0100

    tried to fix font file not being cached for progressive web app offline mode

--- server/frontend/apps/web_app/workbox-config.cjs
+++ server/frontend/apps/web_app/workbox-config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   globDirectory: 'static/',
-  globPatterns: ['**/*.{html,js,css,json,ico,png,svg}'],
+  globPatterns: ['**/*.{html,js,css,json,ico,png,svg,woff,woff2}'],
   swDest: 'static/sw.js',
   runtimeCaching: [
     {
@@ -25,6 +25,17 @@ module.exports = {
         expiration: {
           maxEntries: 50,
           maxAgeSeconds: 30 * 24 * 60 * 60,
+        },
+      },
+    },
+    {
+      urlPattern: ({ request }) => request.destination === 'font',
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'font-cache',
+        expiration: {
+          maxEntries: 20,
+          maxAgeSeconds: 60 * 60 * 24 * 365,
         },
       },
     },
