b0caa069d54d44bf0f0792f88d69d06480256412
glowingkitty <contact@marcobartsch.me>
2025-02-20 20:54:37 +0100

    improved css of signup step 2

--- server/frontend/packages/ui/src/components/signup/steps/step2/Step2TopContent.svelte
+++ server/frontend/packages/ui/src/components/signup/steps/step2/Step2TopContent.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
     import { _ } from 'svelte-i18n';
+    import { fade } from 'svelte/transition';
     export let email: string;
 </script>
 
 <div class="content">
-    <div class="clickable-icon icon_mail"></div>
-    <span class="color-grey-80">{$_('signup.you_received_a_one_time_code_via_email.text')}</span>
-    <mark>{email}</mark>
+    <div class="main-content">
+        <div class="icon-container">
+            <div class="clickable-icon icon_mail signup"></div>
+            <div class="notification-counter" transition:fade={{ delay: 300 }}>1</div>
+        </div>
+        <span class="color-grey-80">{$_('signup.you_received_a_one_time_code_via_email.text')}</span>
+        <mark>{email}</mark>
+    </div>
     <a href="mailto:" class="text-button">
         {$_('signup.open_mail_app.text')}
     </a>
@@ -14,13 +20,55 @@
 
 <style>
     .content {
-        height: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
         padding: 20px;
         text-align: center;
+    }
+
+    .main-content {
+        margin: auto 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
         gap: 16px;
+    }
+
+    .icon-container {
+        position: relative;
+    }
+
+    .notification-counter {
+        position: absolute;
+        top: -8px;
+        right: -8px;
+        width: 35px;
+        height: 35px;
+        background-color: var(--color-button-primary);
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        font-size: 24px;
+    }
+
+    .clickable-icon.icon_mail.signup {
+        cursor: unset;
+        width: 75px;
+        height: 75px;
+    }
+
+    .text-button {
+        position: absolute;
+        bottom: 20px;
+        left: 0;
+        right: 0;
     }
 </style>
